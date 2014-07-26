3f4f52e9820f5231bad8c87fa29419c99f411e69
Steve Schofield <stevebschofield@gmail.com>
2014-07-26 11:22:00 +0100

    Initial Commit

---
+++ BlocBrowser/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BlocBrowser
+//
+//  Created by Steven Schofield on 26/07/2014.
+//  Copyright (c) 2014 Double Digital. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ BlocBrowserTests/BlocBrowserTests.m
@@ -0,0 +1,39 @@
+//
+//  BlocBrowserTests.m
+//  BlocBrowserTests
+//
+//  Created by Steven Schofield on 26/07/2014.
+//  Copyright (c) 2014 Double Digital. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface BlocBrowserTests : XCTestCase
+
+@end
+
+@implementation BlocBrowserTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
